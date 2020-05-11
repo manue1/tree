aca2216ada612fc0b1f87578c5552adb4f5e0a03
manue1 <manuel.bergler@gmail.com>
2020-05-11 19:56:14 +0200

    Move handler to func

--- server.go
+++ server.go
@@ -7,10 +7,19 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-		io.WriteString(w, "Hello, I'm handling this\n")
-	})
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", indexHandler)
+
+	server := &http.Server{
+		Handler: mux,
+		Addr:    ":8000",
+	}
 
 	log.Printf("listening on :8000")
-	log.Fatal(http.ListenAndServe(":8000", nil))
+	log.Fatal(server.ListenAndServe())
+}
+
+func indexHandler(w http.ResponseWriter, req *http.Request) {
+	log.Printf("Request with query: %s\n", req.URL.Query())
+	io.WriteString(w, "Please tell me your favorite tree\n")
 }
